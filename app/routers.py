2c7031da8c75ff509235193307fe1090e93fbe3e
maukka76 <markus.veijola@gmail.com>
2016-01-29 14:52:48 +0200

    Added friends table

--- app/routers.py
+++ app/routers.py
@@ -1,7 +1,9 @@
 from app import app
 #render_template gives you access to Jinja2 template engine
-from flask import render_template,request,make_response,flash
-from app.forms import LoginForm
+from flask import render_template,request,make_response,flash,redirect
+from app.forms import LoginForm,RegisterForm
+from app.db_models import Users
+from app import db
 
 @app.route('/',methods=['GET','POST'])
 def index():
@@ -20,6 +22,22 @@ def index():
 			flash('Give proper information to email and password fields!')
 			return render_template('template_index.html',form=login)
 
+@app.route('/register',methods=['GET','POST'])
+def registerUser():
+	form = RegisterForm()
+	if request.method == 'GET':
+		return render_template('template_register.html',form=form)
+	else:
+		if form.validate_on_submit():
+			user = Users(form.email.data,form.passw.data)
+			db.session.add(user)
+			db.session.commit()
+			flash("Name {0} registered.".format(form.email.data))
+			return redirect('/')
+		else:
+			flash('Invalid email address or no password given')
+			return render_template('template_register.html',form=form)
+		
 @app.route('/user/<name>')
 def user(name):
 	print(request.headers.get('User-Agent'))
